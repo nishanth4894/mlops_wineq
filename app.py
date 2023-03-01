43fd200274c6d8f93ee676489f87a15227c12f31
Bhargav Nishanth <1000072424@hexaware.com>
2023-03-01 10:35:29 +0530

    update prediction_service

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import os
 import yaml
 import joblib
 import numpy as np
+from prediction_service import prediction
 
-params_path = "params.yaml"
+#params_path = "params.yaml"
 webapp_root = "webapp"
 
 static_dir = os.path.join(webapp_root, "static")
@@ -12,29 +13,30 @@ template_dir = os.path.join(webapp_root, "templates")
 
 application = Flask(__name__, static_folder=static_dir,template_folder=template_dir)
 app = application
-def read_params(config_path):
-    with open(config_path) as yaml_file:
-        config = yaml.safe_load(yaml_file)
-    return config
 
-def predict(data):
-    config = read_params(params_path)
-    model_dir_path = config["webapp_model_dir"]
-    model = joblib.load(model_dir_path)
-    prediction = model.predict(data)
-    print(prediction)
-    return prediction[0]
+#def read_params(config_path):
+#    with open(config_path) as yaml_file:
+#        config = yaml.safe_load(yaml_file)
+#    return config
 
-def api_response(request):
-    try:
-        data = np.array([list(request.json.values())])
-        response = predict(data)
-        response = {"response": response}
-        return response
-    except Exception as e:
-        print(e)
-        error = {"error": "Something went wrong!! Try again later!"}
-        return error
+#def predict(data):
+#    config = read_params(params_path)
+#    model_dir_path = config["webapp_model_dir"]
+#    model = joblib.load(model_dir_path)
+#    prediction = model.predict(data)
+#    print(prediction)
+#    return prediction[0]
+
+#def api_response(request):
+#    try:
+#        data = np.array([list(request.json.values())])
+#        response = predict(data)
+#        response = {"response": response}
+#        return response
+#    except Exception as e:
+#        print(e)
+#        error = {"error": "Something went wrong!! Try again later!"}
+#        return error
 
 
 
@@ -44,20 +46,20 @@ def index():
     if request.method == "POST":
         try:
             if request.form:
-                data = dict(request.form).values()
-                data = [list(map(float, data))]
-                response = predict(data)
+                data_req = dict(request.form)
+                response = prediction.form_response(data_req)
                 return render_template("index.html", response=response)
+
             elif request.json:
-                response = api_response(request)
+                response = prediction.api_response(request.json)
                 return jsonify(response)
 
         except Exception as e:
             print(e)
-            error = {"error": "Something went wrong!! Try again later!"}
+            #error = {"error": "Something went wrong!! Try again later!"}
             error = {"error": e}
 
-            return render_template("404.html", error=error)
+            return render_template("404.html", error=e)
     else:
         return render_template("index.html")
 
